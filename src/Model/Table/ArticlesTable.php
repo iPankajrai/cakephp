6f59ebdd18f8b878bb4a1e2a2ce3a17ebed0ceaf
Pankaj Rai <pankaj200497@gmail.com>
2021-02-09 11:43:58 +0530

    First Commit in cakephp

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,40 @@
+<?php
+// src/Model/Table/ArticlesTable.php
+namespace App\Model\Table;
+
+// the Validator class
+use Cake\Validation\Validator;
+use Cake\ORM\Table;
+// the Text class
+use Cake\Utility\Text;
+
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+	{
+	    if ($entity->isNew() && !$entity->slug) {
+	        $sluggedTitle = Text::slug($entity->title);
+	        // trim slug to maximum length defined in schema
+	        $entity->slug = substr($sluggedTitle, 0, 191);
+	    }
+	}
+
+	public function validationDefault(Validator $validator)
+	{
+	    $validator
+	        ->allowEmptyString('title', false)
+	        ->minLength('title', 10)
+	        ->maxLength('title', 255)
+
+	        ->allowEmptyString('body', false)
+	        ->minLength('body', 10);
+
+	    return $validator;
+	}
+}
